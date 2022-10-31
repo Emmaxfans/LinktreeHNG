8ddc0082a6182bd2d51b979d6fa5cc510fb864f9
Emmanuel Adewoye <emmaxfans@gmail.com>
2022-10-31 15:45:59 +0100

    changes to the achor tag

--- src/App.js
+++ src/App.js
@@ -4,90 +4,71 @@ function App() {
   return (
     <div className="App container">
 
-      <img src='./images/share.png' alt='share-linktree' className='share-img'/>
-      <img src='./images/more.png' alt='share-linktree' className='share1-img'/>
 
-      <div className='text-center'>
-      <img src='./images/profile-img.png' alt='profile-img' className='profile-img text-center' id='profile__img'/>
-      <p className='p1'>Emmanuel Adewoye</p>
+      <div className='profile'>
+      <img src='./images/profile.png' alt='profile-img' className='profile-img  img-fluid' id='profile__img'/>
+      <p className='p1'>@Emmax</p>
+
+      <div className='share-img'>
+        <i className="fa fa-share share-icon" aria-hidden="true"></i>
+      </div>
       </div>
 
       <div className='container'>
         <div className='row'>
-          <div className='col-lg-12 md-6 sm-6  con1'>
-            <p className='pt-4 text-center' id='twitter'>
-              <a href='https://twitter.com/_Emmaxfans'>
-            Twitter Link
-            </a>
-            </p>
-            
+          <div className='col-lg-12 md-6 sm-6  con1'> 
+              <a href='https://twitter.com/_Emmaxfans' id='twitter'>
+               <p className='text-center padding-con'>Twitter Link</p>
+              </a>          
           </div>
 
         </div>
 
         <div className='row'>
           <div className='col-lg-12 md-6 sm-6  con1'>
-            <p className='pt-4 text-center' id='btn__zuri'>
-              <a href='https://training.zuri.team'>
-            Zuri Team
-            </a>
-            </p>
             
+              <a href='https://training.zuri.team' id='btn__zuri'>
+              <p className='text-center padding-con'>Zuri Team</p>
+            
+            </a>  
           </div>
-
         </div>
 
         <div className='row'>
           <div className='col-lg-12 md-6 sm-6  con1'>
-            <p className='pt-4 text-center' id='books'>
-              <a href='https://books.zuri.team/'>
-            Zuri Books
-            </a>
-            </p>
-            
+              <a href='https://books.zuri.team/' id='books'>
+               <p className='text-center padding-con'>Zuri Books</p>
+              </a>   
           </div>
-
         </div>
 
         <div className='row'>
           <div className='col-lg-12 md-6 sm-6  con1'>
-            <p className='pt-4 text-center' id='books__python'>
-              <a href='https://books.zuri.team/python-for-beginners?ref_id=<Emmanuel Adewoye>'>
-            Python Books
-            </a>
-            </p>
-            
+              <a href='https://books.zuri.team/python-for-beginners?ref_id=<Emmanuel Adewoye>' id='books__python'>
+               <p className='text-center padding-con'>Python Books</p>
+              </a>  
           </div>
-
         </div>
 
         <div className='row'>
           <div className='col-lg-12 md-6 sm-6  con1'>
-            <p className='pt-4 text-center' id='pitch'>
-              <a href='https://background.zuri.team/'>
-            Background Check for coders
-            </a>
-            </p>
-            
+              <a href='https://background.zuri.team' id='pitch'>
+               <p className='text-center padding-con'>Background Check for coders</p>
+              </a>  
           </div>
-
         </div>
 
         <div className='row'>
           <div className='col-lg-12 md-6 sm-6  con1'>
-            <p className='pt-4 text-center' id='book_design'>
-              <a href='https://books.zuri.team/design-rules'>
-            Design Books
-            </a>
-            </p>
-            
+              <a href='https://books.zuri.team/design-rules' id='book_design'>
+               <p className='text-center padding-con'>Design Books</p>
+              </a>   
           </div>
-
         </div>
       </div>
 
       <div className='text-center con2'>
-        <a href='https://app.slack.com/'>
+        <a href='https://app.slack.com/client/T042F7V19Q8/C041JU70S5U/rimeto_profile/U048PA3FA66' id='Emmax'>
         <img src='./images/slack.png' alt='slack-logo' className='slack-img'/>
         </a>
         <a href='https://github.com/Emmaxfans'>
@@ -99,9 +80,7 @@ function App() {
       <div className='container-fluid con3'>
         <div className='row'>
           <div className='col-lg-4 md-4 sm-4'>
-
-          <a href='/#'><img src='./images/zuri1.svg' alt='zuri-logo' className='zuri-img'/></a>
-
+          <img src='./images/zurii.png' alt='zuri-logo' className='zuri-img'/>
           </div>
 
           <div className='col-lg-4 md-4 sm-4'>
@@ -109,7 +88,7 @@ function App() {
           </div>
 
           <div className='col-lg-4 md-4 sm-4'>
-          <a href='/#'><img src='./images/ingressive.webp' alt='slack-logo' className='ingressive-img img-fluid'/></a>
+          <img src='./images/logo2.b6de37f7d5d4c7f03d0.png' alt='slack-logo' className='ingressive-img img-fluid'/>
           </div>
 
 
